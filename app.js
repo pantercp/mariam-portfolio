060533ae2214b5bff5bff28c21274b659dd48b83
Christian Panter <pantercp@outlook.com>
2023-01-26 14:50:21 +0000

    First commit, cjpixel portfolio template

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// -----------Header--------
+
+const burgerBtn = document.querySelector('.row-burger');
+const dropMenu = document.querySelector('.drop-menu');
+
+let menuOpen = false;
+burgerBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    burgerBtn.classList.add('open');
+    dropMenu.classList.add('open');
+    menuOpen = true;
+  } else {
+    burgerBtn.classList.remove('open');
+    dropMenu.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
