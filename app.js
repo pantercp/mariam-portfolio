57ba700cd4a2c8dcba40bd5faab79445ecd14f6b
Christian Panter <pantercp@outlook.com>
2023-10-09 00:48:18 +0100

    Coding for testimonials plus loads of styling for nav

--- app.js
+++ app.js
@@ -17,3 +17,48 @@ burgerBtn.addEventListener('click', () => {
   }
 });
 
+// Intersection For Fader
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0.3,
+};
+
+const appearOnScroll = new IntersectionObserver
+  (function (entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    })
+  },
+    appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+// Carousel Buttons
+
+const nextbtn = document.querySelector(".nextbutton");
+const testimonials = document.querySelector(".testimonials");
+const testCount = testimonials.childElementCount;
+// const testimonials = document.querySelectorAll(".testimonial");
+
+
+// for(let testimonial of testimonials){
+//   // testimonial = tes
+//   console.log(testimonial)
+// }
+
+nextbtn.addEventListener('click', () => {
+  console.log("clicked");
+  // console.log({testimonials}[0]);
+
+});
+
+
